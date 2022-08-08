96d5c1af5bad9aca10062b923b2c7259dd4d20b7
sonarweb <axel.hstr@gmail.com>
2022-08-07 22:38:52 -0500

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import styles from '../styles/Navbar.module.css'
+import Image from 'next/dist/client/image'
+import Link from 'next/link'
+import { AiOutlineClose, AiOutlineFacebook, AiOutlineInstagram, AiOutlineMenu, AiOutlineYoutube } from "react-icons/ai";
+import { FaFacebook, FaFacebookF, FaInstagram, FaYoutube, IconName } from "react-icons/fa";
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleNav = () =>{
+        setNav(!nav)
+    }
+
+  return (
+    <div className={styles.container}>
+        <div className={styles.nav}>
+            <Image 
+            src='/../public/assets/logo-rotten-pork-white.png'
+            alt='/'
+            width={118.75}
+            height={85.5}
+            />
+            <div>
+                <ul className={styles.list}>
+                    <Link href='/'>
+                        <li className={styles.listItem}>Inicio</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className={styles.listItem}>Biografía</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className={styles.listItem}>Discografía</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className={styles.listItem}>Contacto</li>
+                    </Link>
+                </ul>
+                <div onClick={handleNav} className={styles.hamburguer}>
+                    <AiOutlineMenu />
+                </div>
+            </div>
+        </div>
+            <div className={nav ? styles.sidebarExpanded : styles.sidebar}>
+                <div>
+                    <div className={styles.sidebarHead}>
+                        <div onClick={handleNav}>
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+                    <div className={styles.sidebarLogo}>
+                    <Image 
+                        src='/../public/assets/logo-rotten-pork-red.png' 
+                        alt='/'
+                        width={230}
+                        height={165.6}
+                        />
+                    </div>
+                </div>
+                <div>
+                <ul className={styles.sideBarList}>
+                    <Link href='/'>
+                        <li className={styles.sideListItem}>Inicio</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className={styles.sideListItem}>Biografía</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className={styles.sideListItem}>Discografía</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className={styles.sideListItem}>Contacto</li>
+                    </Link>
+                </ul>
+                <div className={styles.sideFooter}>
+                    <p className={styles.cta}>Síguenos</p>
+                    <div className={styles.socialItems}>
+                        <div>
+                            <FaFacebookF />
+                        </div>
+                        <div>
+                            <FaInstagram />
+                        </div>
+                        <div>
+                            <FaYoutube />
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
